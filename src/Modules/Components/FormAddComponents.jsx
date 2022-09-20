b6b93697a5c84cae1e711233aa38b940cdea2162
huynq08032000 <55910030+huynq08032000@users.noreply.github.com>
2022-09-20 16:59:22 +0700

    add redux, formik for FormAdd

--- src/Modules/Components/FormAddComponents.jsx
+++ src/Modules/Components/FormAddComponents.jsx
@@ -3,90 +3,86 @@ import '../Css/Component.css'
 import 'antd/dist/antd.css';
 import { Button, Input, Row, Col, message, Typography } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import { v4 as uuidv4} from 'uuid'
-import { validateForm, validForm } from '../ultils/validate';
+import { v4 as uuidv4 } from 'uuid'
 import { useDispatch, useSelector } from 'react-redux';
 import { addTodo } from '../redux/todoSlice';
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object({
+    name: yup
+        .string("Enter name")
+        .required("Name is required"),
+    des: yup
+        .string("Enter description")
+        .required("Description is required")
+});
 
 const FormAddComponent = () => {
     const key = 'updatable';
     const { Text } = Typography;
-    const [formValue, setFormValue] = useState({ name: '', des: '', checked: false })
-    const [validateValues, setValidateValues] = useState({});
     const [loading, setLoading] = useState(false)
     // const [state, dispatch] = useContext(Context);
     const todoList = useSelector(state => state.todoList.todoList)
     const dispatch = useDispatch()
-    const handleAddItem = () => {
-        const validateValues = validateForm(formValue)
-        setValidateValues(validateValues)
-        if (!validForm(validateValues)) {
-            return;
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            des: "",
+            checked : false,
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            handleAddItem(values)
         }
+    })
+    const myHandleChange = (event) => {
+        formik.handleChange(event);
+    };
+    const handleAddItem = (values) => {
         // Add to globalstate, add localstorerage
         message.loading({
             content: 'Adding...',
             key,
-          },100);
+        }, 100);
         setLoading(true)
         setTimeout(() => {
             const arr = [...todoList]
-            arr.push({...formValue, id : uuidv4()})
+            arr.push({ ...values, id: uuidv4() })
             localStorage.setItem('todoList', JSON.stringify(arr)) //add localstorage
-            dispatch(addTodo({...formValue, id : uuidv4()})) //add to global state
+            dispatch(addTodo({ ...values, id: uuidv4() })) //add to global state
             message.success({
-                content : 'Adding success',
+                content: 'Adding success',
                 key,
             });
-            setLoading(false)
-            setFormValue(prevForm => {
-                return {
-                    ...prevForm,
-                    name: '',
-                    des: '',
-                }
-            })
-            setValidateValues({})
+            setLoading(false)        
+            formik.handleReset()
         }, 1000)
     }
     return (
         <div className='form-add-container'>
-            <Row>
-                <Col span={10} offset={6}>
-                    <div className='form-container'>
-                        <div className='input-wrapper'>
-                            <Input placeholder='Name' size='large' value={formValue.name}
-                                onChange={(e) => {
-                                    setFormValue((prevForm) => {
-                                        return {
-                                            ...prevForm,
-                                            name: e.target.value
-                                        }
-                                    })
-                                }} />
-                            {validateValues && <Text type="danger">{validateValues.nameErr}</Text>}
+            <form>
+                <Row>
+                    <Col span={10} offset={6}>
+                        <div className='form-container'>
+                            <div className='input-wrapper'>
+                                <Input placeholder='Name' size='large' name='name' value={formik.values.name} onChange={myHandleChange} />
+                                {formik.touched.name && <Text type="danger">{formik.errors.name}</Text>}
+                            </div>
+                            <div className='input-wrapper'>
+                                <Input placeholder='Description' size='large' name='des' value={formik.values.des} onChange={myHandleChange} />
+                                {formik.touched.des && <Text type="danger">{formik.errors.des}</Text>}
+                            </div>
                         </div>
-                        <div className='input-wrapper'>
-                            <Input placeholder='Description' size='large' value={formValue.des}
-                                onChange={(e) => {
-                                    setFormValue((prevForm) => {
-                                        return {
-                                            ...prevForm,
-                                            des: e.target.value
-                                        }
-                                    })
-                                }} />
-                            {validateValues && <Text type="danger">{validateValues.desErr}</Text>}
+                    </Col>
+                    <Col span={2}>
+                        <div className='btn-submit-form'>
+                            <Button onClick={formik.handleSubmit} icon={<PlusOutlined />} loading={loading} type='danger'>Add new item</Button>
                         </div>
-                    </div>
-                </Col>
-                <Col span={2}>
-                    <div className='btn-submit-form'>
-                        <Button onClick={handleAddItem} type='danger' icon={<PlusOutlined />} loading={loading}>Add new item</Button>
-                    </div>
-                </Col>
-            </Row>
-        </div>
+                    </Col>
+                </Row>
+            </form>
+        </div >
     )
 }
 
